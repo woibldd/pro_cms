655d271e8d44eddf6948b9210a20685d27ce58b5
caoshiyuan <codesircao@126.com>
2021-06-16 20:40:35 +0800

    add:build后代码

---
+++ src/.nuxt/router.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _000c4fbd = () => interopDefault(import('../pages/article/detail/_id.vue' /* webpackChunkName: "pages/article/detail/_id" */))
+const _7cf85b62 = () => interopDefault(import('../pages/_id.vue' /* webpackChunkName: "pages/_id" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/article/detail/:id?",
+    component: _000c4fbd,
+    name: "article-detail-id"
+  }, {
+    path: "/:id?",
+    component: _7cf85b62,
+    name: "id"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
---
+++ src/.nuxt/router.scrollBehavior.js
@@ -0,0 +1,5 @@
+
+
+export default function (to, from, savedPosition) {
+      console.log({ to, from, savedPosition });
+    }
