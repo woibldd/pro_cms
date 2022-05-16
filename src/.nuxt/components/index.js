4070326abb4230c330bc838a7c9eeb2ec72d229f
caoshiyuan <codesircao@126.com>
2022-05-17 01:08:13 +0800

    fix: build

--- src/.nuxt/components/index.js
+++ src/.nuxt/components/index.js
@@ -9,6 +9,7 @@ export { default as BlindboxBlindStrategy } from '../../components/blindbox/blin
 export { default as BlindboxBlindTimeText } from '../../components/blindbox/blindTimeText.vue'
 export { default as BlindboxCreatePoster } from '../../components/blindbox/createPoster.vue'
 export { default as BlindboxTitleImage } from '../../components/blindbox/titleImage.vue'
+export { default as Trading } from '../../components/trading/index.vue'
 export { default as PolygonAirdropAddressCard } from '../../components/polygon/AirdropAddressCard.vue'
 export { default as PolygonAirdropAwardCard } from '../../components/polygon/AirdropAwardCard.vue'
 export { default as PolygonInvitedCard } from '../../components/polygon/InvitedCard.vue'
@@ -18,7 +19,6 @@ export { default as PolygonMintSuccessCard } from '../../components/polygon/Mint
 export { default as PolygonWhitelistcard } from '../../components/polygon/Whitelistcard.vue'
 export { default as PolygonCVueCountdown } from '../../components/polygon/c-vue-countdown.vue'
 export { default as PolygonCountdown } from '../../components/polygon/countdown.vue'
-export { default as Trading } from '../../components/trading/index.vue'
 export { default as VueCreatePosterLayoutCanvasPoster } from '../../components/vue-create-poster/layout/canvas-poster.vue'
 export { default as VueCreatePosterLayoutGradient } from '../../components/vue-create-poster/layout/gradient.js'
 export { default as VueCreatePosterLayoutHeader } from '../../components/vue-create-poster/layout/header.vue'
@@ -36,6 +36,7 @@ export const LazyBlindboxBlindStrategy = import('../../components/blindbox/blind
 export const LazyBlindboxBlindTimeText = import('../../components/blindbox/blindTimeText.vue' /* webpackChunkName: "components/blindbox-blind-time-text" */).then(c => wrapFunctional(c.default || c))
 export const LazyBlindboxCreatePoster = import('../../components/blindbox/createPoster.vue' /* webpackChunkName: "components/blindbox-create-poster" */).then(c => wrapFunctional(c.default || c))
 export const LazyBlindboxTitleImage = import('../../components/blindbox/titleImage.vue' /* webpackChunkName: "components/blindbox-title-image" */).then(c => wrapFunctional(c.default || c))
+export const LazyTrading = import('../../components/trading/index.vue' /* webpackChunkName: "components/trading" */).then(c => wrapFunctional(c.default || c))
 export const LazyPolygonAirdropAddressCard = import('../../components/polygon/AirdropAddressCard.vue' /* webpackChunkName: "components/polygon-airdrop-address-card" */).then(c => wrapFunctional(c.default || c))
 export const LazyPolygonAirdropAwardCard = import('../../components/polygon/AirdropAwardCard.vue' /* webpackChunkName: "components/polygon-airdrop-award-card" */).then(c => wrapFunctional(c.default || c))
 export const LazyPolygonInvitedCard = import('../../components/polygon/InvitedCard.vue' /* webpackChunkName: "components/polygon-invited-card" */).then(c => wrapFunctional(c.default || c))
@@ -45,7 +46,6 @@ export const LazyPolygonMintSuccessCard = import('../../components/polygon/MintS
 export const LazyPolygonWhitelistcard = import('../../components/polygon/Whitelistcard.vue' /* webpackChunkName: "components/polygon-whitelistcard" */).then(c => wrapFunctional(c.default || c))
 export const LazyPolygonCVueCountdown = import('../../components/polygon/c-vue-countdown.vue' /* webpackChunkName: "components/polygon-c-vue-countdown" */).then(c => wrapFunctional(c.default || c))
 export const LazyPolygonCountdown = import('../../components/polygon/countdown.vue' /* webpackChunkName: "components/polygon-countdown" */).then(c => wrapFunctional(c.default || c))
-export const LazyTrading = import('../../components/trading/index.vue' /* webpackChunkName: "components/trading" */).then(c => wrapFunctional(c.default || c))
 export const LazyVueCreatePosterLayoutCanvasPoster = import('../../components/vue-create-poster/layout/canvas-poster.vue' /* webpackChunkName: "components/vue-create-poster-layout-canvas-poster" */).then(c => wrapFunctional(c.default || c))
 export const LazyVueCreatePosterLayoutGradient = import('../../components/vue-create-poster/layout/gradient.js' /* webpackChunkName: "components/vue-create-poster-layout-gradient" */).then(c => wrapFunctional(c.default || c))
 export const LazyVueCreatePosterLayoutHeader = import('../../components/vue-create-poster/layout/header.vue' /* webpackChunkName: "components/vue-create-poster-layout-header" */).then(c => wrapFunctional(c.default || c))
